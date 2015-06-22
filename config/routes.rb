ba4b6145a0d9fb4c7739a1c641731d2b17a018d9
Rathanak Sreang <sreang.rathanak@framgia.com>
2015-06-22 16:27:04 +0700

    Store admin page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :galleries do
     resources :photos
   end
+  namespace :admin do
+    root "static_pages#dashboard"
+    resources :pages
+    resources :galleries
+    resources :photos    
+  end
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
