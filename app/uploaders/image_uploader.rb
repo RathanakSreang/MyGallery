52da875f1cecc6e8c45fa9680540ae643bdd537a
Rathanak Sreang <sreang.rathanak@framgia.com>
2016-03-28 09:43:16 +0700

    add text to image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,10 +35,25 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :large do
     process :resize_and_pad => [900, 500, :transparent, "Center"]
+    process :add_text
   end
 
   version :thumb do
     process :resize_and_pad => [400, 400, :transparent, "Center"]
+    process :add_text
+  end
+
+  def add_text
+    manipulate! do |image|
+        # binding.pry
+      image.combine_options do |c|
+        c.gravity 'Center'
+        c.pointsize '80'
+        c.annotate('+0+0', "Visit Us at: Kimly")
+        c.fill 'grey'
+      end
+      image
+    end
   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
